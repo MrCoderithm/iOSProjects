ad029eab66d4927bcb3b0b640171dd7d3b56eceb
Ali Muhammad <theysee@live.ca>
2021-08-08 19:59:56 -0400

    Club House Swift UI
    First Commit

---
+++ clubhouse/Clubapartment/Source/ClubapartmentApp.swift
@@ -0,0 +1,17 @@
+//
+//  ClubapartmentApp.swift
+//  Clubapartment
+//
+//  Created by Ali Muhammad on 16/02/2021.
+//
+
+import SwiftUI
+
+@main
+struct ClubapartmentApp: App {
+    var body: some Scene {
+        WindowGroup {
+            HomeView()
+        }
+    }
+}
---
+++ clubhouse/Clubapartment/Source/Helpers/Names.swift
@@ -0,0 +1,17 @@
+//
+//  Names.swift
+//  Clubapartment
+//
+//  Created by Ali Muhammad on 16/02/2021.
+//
+
+import Foundation
+
+struct Names {
+    static let names = [
+        "###", "Ali Muhammad", "Arianne Kellman", "Jennifer Doe", "James Grak.", "Nick Sunts", "Sara Kemeni",
+        "Will Kqiyl", "Adaobi Njo", "Nikita Grant", "Stephane Nouz", "Manal Arf", "Elijah Rstand", "Lily S.",
+        "Maddison Pete", "Savanah Swink", "NHdzana Davidson", "Angela Ross", "Nath Pulley", "Henry Mbomgo",
+        "Stan Mbomgo", "Elsie Fonka", "Maire Louise"
+    ]
+}
