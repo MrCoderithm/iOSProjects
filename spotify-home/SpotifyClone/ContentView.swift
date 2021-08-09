5b6567b5babebf612114bc9cdab1ec8e057f9576
Ali Muhammad <theysee@live.ca>
2021-08-08 20:05:04 -0400

    Spotify SwiftUI
    Images and Spotify UI

---
+++ spotify-home/SpotifyClone/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  SpotifyClone
+//
+//  Created by Ali Muhammad on 03/08/2020.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    
+    var body: some View {
+        return TabBar()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
+
+
+
---
+++ spotify-home/SpotifyClone/SpotifyCloneApp.swift
@@ -0,0 +1,17 @@
+//
+//  SpotifyCloneApp.swift
+//  SpotifyClone
+//
+//  Created by Ali Muhammad on 03/08/2020.
+//
+
+import SwiftUI
+
+@main
+struct SpotifyCloneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
